b1f8738cb0c9dee2366cf7fae188bce6e31ad29c
LenSin3 <lenpk3@yahoo.co.uk>
2020-10-25 19:45:17 -0700

    Completed precipitation api to return dict of date and prcp in the flask app

---
+++ app.py
@@ -0,0 +1,69 @@
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+
+    # Calculate the date 1 year ago from the last data point in the database
+    date_delta = dt.date(2017, 8, 23) - dt.timedelta(days = 365.25)
+    print("date_delta :", date_delta)
+
+    # Perform a query to retrieve the data and precipitation scores
+    data_precip = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= date_delta).all()
+
+    session.close()
+
+    all_precipitation = []
+    for date, prcp in data_precip:
+        precipitation_dict = {}
+        precipitation_dict['date'] = date
+        precipitation_dict['prcp'] = prcp
+        all_precipitation.append(precipitation_dict)
+
+    return jsonify(all_precipitation)
