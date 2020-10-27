299c5efeac4e2b346dd8e25eb8e5fe3d8bd12494
LenSin3 <lenpk3@yahoo.co.uk>
2020-10-27 13:13:59 -0700

    Completed docstrings for the functions in the climate app api

--- app.py
+++ app.py
@@ -47,15 +47,14 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
+
+    """Create session (link) from Python to the DB"""
     session = Session(engine)
 
-
-    # Calculate the date 1 year ago from the last data point in the database
+    """Calculate the date 1 year ago from the last data point in the database"""
     date_delta = dt.date(2017, 8, 23) - dt.timedelta(days = 365.25)
-    print("date_delta :", date_delta)
 
-    # Perform a query to retrieve the data and precipitation scores
+    """Perform a query to retrieve the data and precipitation scores"""
     data_precip = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >= date_delta).all()
 
@@ -72,12 +71,12 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
+    """Create session (link) from Python to the DB"""
     session = Session(engine)
 
     
 
-    # Query list of distinct stations in the dataset
+    """Query list of distinct stations in the dataset"""
     stations_dstnct = session.query(distinct(Measurement.station)).all()
 
     session.close()
@@ -98,16 +97,15 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def temp_obs():
-    # Create our session (link) from Python to the DB
+    """Create session (link) from Python to the DB"""
     session = Session(engine)
 
-    # Query the dates and temperature observations of the most active station for the last year of data
-    # List the stations and the counts in descending order.
+    """Query the dates and temperature observations of the most active station for the last year of data"""
 
-    # Time delta of 1 year for latest date for which observation was made
+    """Time delta of 1 year for latest date for which observation was made"""
     most_active_date_delta = dt.date(2017, 8, 18) - dt.timedelta(days = 365.25)
 
-    # Dates and temperatures of 1 year time delta at station with highest number of temperature observations
+    """Dates and temperatures of 1 year time delta at station with highest number of temperature observations"""
     most_active_date_delta_tobs = session.query(Measurement.date, Measurement.tobs).\
     filter(Measurement.station == 'USC00519281').\
     filter(Measurement.date >= most_active_date_delta).all()
@@ -128,7 +126,10 @@ def temp_obs():
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
+    """Create session (link) from Python to the DB"""
     session = Session(engine)
+
+    """Query the minimum temperature, the average temperature, and the max temperature for a given start date"""
     sel = [Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
     date_start_stats = session.query(*sel).filter(Measurement.date >= start).group_by(Measurement.date).all()
 
@@ -153,8 +154,10 @@ def start_date(start):
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_date(start, end):
-
+    """Create session (link) from Python to the DB"""
     session = Session(engine)
+
+    """Query the minimum temperature, the average temperature, and the max temperature for a given start and end date"""
     sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
     date_start_end_stats = session.query(*sel).filter(Measurement.date >= start).\
         filter(Measurement.date <= end).group_by(Measurement.date).all()
